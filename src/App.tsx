29c586666aa59350f433c9a7ac23e74336557bf3
Pierre Zimmermann <pierre.zimmermann@smail.inf.h-brs.de>
2022-06-13 22:55:35 +0200

    inital commit App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router,
+Routes, Route} from "react-router-dom"
+import '@elastic/eui/dist/eui_theme_light.css';
+import {EuiProvider, EuiHeader,
+    EuiHeaderSectionItem,
+    EuiHeaderLogo,
+    EuiHeaderLink,EuiHeaderLinks} from "@elastic/eui";
 
-function App() {
+import Home from "./pages/Home/Home";
+import AboutPage from "./pages/About/AboutPage";
+import Resources from "./pages/Resources/Resources";
+import Error from "./pages/Error/Error";
+import SamplePage from "./pages/SamplePage";
+import Term from "./pages/Term/Term";
+import Footer from "./components/Footer/Footer";
+
+import "./App.css"
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <EuiProvider colorMode={"light"}>
+      <Router>
+          <div>
+          <div>
+              <EuiHeader>
+                  <EuiHeaderSectionItem border="right">
+                      <EuiHeaderLogo iconType="searchProfilerApp">SemLookP</EuiHeaderLogo>
+                  </EuiHeaderSectionItem>
+                  <EuiHeaderSectionItem>
+                      <EuiHeaderLinks>
+                          <EuiHeaderLink href="/">Home</EuiHeaderLink>
+                          <EuiHeaderLink href="/resources">Resources</EuiHeaderLink>
+                          <EuiHeaderLink href="/about">About</EuiHeaderLink>
+                          <EuiHeaderLink href="/sample">Sample</EuiHeaderLink>
+                      </EuiHeaderLinks>
+                  </EuiHeaderSectionItem>
+              </EuiHeader>
+          </div>
+              <div className="mainPage" >
+         <Routes>
+             <Route path="/" element={<Home/>}/>
+             <Route path="/about" element={<AboutPage/>}/>
+             <Route path="/resources" element={<Resources/>}/>
+             <Route path="/sample" element={<SamplePage/>}/>
+             <Route path="/terms" element={<Term/>}/>
+            <Route path="*" element={<Error/>} />
+         </Routes>
+              </div>
+          <div>
+              <Footer/>
+          </div>
+          </div>
+      </Router>
+      </EuiProvider>
+  )
 }
 
-export default App;
+export default App
