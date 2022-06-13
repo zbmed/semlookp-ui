2c5711253962699fd18ad47f9f46b4a88227c742
Pierre Zimmermann <pierre.zimmermann@smail.inf.h-brs.de>
2022-06-13 23:00:17 +0200

    added Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {EuiFlexGroup, EuiFlexItem, EuiPanel} from "@elastic/eui";
+
+export default function Footer() {
+    return (
+        <div>
+            <EuiPanel className="footer"
+            hasBorder={true}>
+                <EuiFlexGroup gutterSize={"none"}>
+                    <EuiFlexItem>About the service:</EuiFlexItem>
+                    <EuiFlexItem>Maintainers:</EuiFlexItem>
+                    <EuiFlexItem>Project Partners:</EuiFlexItem>
+                    <EuiFlexItem>Funding:</EuiFlexItem>
+                </EuiFlexGroup>
+            </EuiPanel>
+        </div>
+    )
+
+}
