d449d599cc6aaefb0085ea46a5ff64998c0fe1d3
Pierre Zimmermann <pierre.zimmermann@smail.inf.h-brs.de>
2022-08-08 23:42:24 +0200

    added SearchWidget to landing page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,26 @@
-import React from "react";
-import {SearchWidget} from "../../components/Widget/SearchWidget/SearchWidget"
-import {EuiFlexItem} from "@elastic/eui";
+import React, {useState} from "react";
+import {EuiFlexItem, EuiPanel, EuiText} from "@elastic/eui";
 import './Home.css'
-
+import {SearchWidget} from "@km/widgets-semlookp";
+import {useNavigate} from "react-router-dom";
 
 export default function Home (){
+    const [searchResults, setSearchResults] = useState([]);
+    let navigate = useNavigate()
     return(
         <div>
             <div className="searchbar">
             <h1>Welcome to the Terminology Service</h1>
             <EuiFlexItem className="searchBar">
-            <SearchWidget api={'https://semanticlookup.zbmed.de/ols/api/'}/>
+                <SearchWidget api={'https://semanticlookup.zbmed.de/ols/api/'} onChange={setSearchResults}/>
+                {searchResults.map(result => <EuiPanel style={{padding: '10px', margin: '10px'}}><EuiText onClick={() =>
+                    navigate({
+                        pathname: "/terms",
+                        search: "?iri=" + result.value.iri.toString(),
+                    })
+                }>{result.label} {result.value.hasOwnProperty('description') ?
+                    ' : ' + result.value.description :
+                    '' }</EuiText></EuiPanel>)}
             </EuiFlexItem>
             </div>
         </div>
