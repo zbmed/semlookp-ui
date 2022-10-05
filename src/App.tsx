188c1c1405281104969da54f2da43e7dfc6bf1c1
Vera Clemens <clemens@zbmed.de>
2022-10-05 17:32:33 +0200

    feat: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,23 @@ import "@elastic/eui/dist/eui_theme_light.css";
 import {
   EuiProvider,
   EuiHeader,
+  EuiHeaderSection,
   EuiHeaderSectionItem,
   EuiHeaderLogo,
   EuiHeaderLink,
   EuiHeaderLinks,
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiSpacer,
+  EuiShowFor,
+  EuiFlexGroup,
+  EuiFlexItem,
 } from "@elastic/eui";
 import {QueryClientProvider, QueryClient} from "react-query"
+import { ReactComponent as SEMLOOKPLOGO } from "./components/Logos/NFDI_SemLookP_Logo.svg";
+import { ReactComponent as NFDI4HEALTHICON } from "./components/Logos/NFDI4Health_Icon.svg";
 
 import Home from "./pages/Home/Home";
 import AboutPage from "./pages/About/AboutPage";
@@ -26,38 +37,60 @@ function App() {
       <EuiProvider colorMode={"light"}>
         <QueryClientProvider client={queryClient}>
           <Router basename="/dev">
-            <div>
-              <div>
-                <EuiHeader>
-                  <EuiHeaderSectionItem border="right">
-                    <EuiHeaderLogo iconType="searchProfilerApp">
-                      SemLookP
-                    </EuiHeaderLogo>
-                  </EuiHeaderSectionItem>
-                  <EuiHeaderSectionItem>
-                    <EuiHeaderLinks>
-                      <EuiHeaderLink href="/">Home</EuiHeaderLink>
-                      <EuiHeaderLink href="/resources">Resources</EuiHeaderLink>
-                      <EuiHeaderLink href="/about">About</EuiHeaderLink>
-                      <EuiHeaderLink href="/sample">Sample</EuiHeaderLink>
-                    </EuiHeaderLinks>
-                  </EuiHeaderSectionItem>
-                </EuiHeader>
-              </div>
-              <div className="mainPage">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/about" element={<AboutPage />} />
-                  <Route path="/resources" element={<Resources />} />
-                  <Route path="/sample" element={<SamplePage />} />
-                  <Route path="/terms" element={<Term />} />
-                  <Route path="*" element={<Error />} />
-                </Routes>
-              </div>
-              <div>
-                <Footer />
-              </div>
-            </div>
+            <EuiPage paddingSize="none">
+              <EuiPageBody>
+                <EuiPageContent
+                  hasBorder={false}
+                  hasShadow={false}
+                  paddingSize="none"
+                  color="transparent"
+                  borderRadius="none"
+                >
+                  <EuiHeader>
+                    <EuiHeaderSection side="left">
+                      <EuiHeaderSectionItem border="right">
+                        <EuiHeaderLogo iconType={NFDI4HEALTHICON} iconTitle="SemLookP" href="#" />
+                      </EuiHeaderSectionItem>
+                      <EuiHeaderSectionItem>
+                        <EuiHeaderLinks>
+                          <EuiHeaderLink href="/">Home</EuiHeaderLink>
+                          <EuiHeaderLink href="/resources">Resources</EuiHeaderLink>
+                          <EuiHeaderLink href="/about">About</EuiHeaderLink>
+                          <EuiHeaderLink href="/sample">Sample</EuiHeaderLink>
+                        </EuiHeaderLinks>
+                      </EuiHeaderSectionItem>
+                    </EuiHeaderSection>
+                  </EuiHeader>
+                  <EuiPageContentBody restrictWidth>
+                    <EuiSpacer size="xl" />
+                    <EuiShowFor sizes={["xl"]}>
+                      <EuiSpacer size="xl" />
+                    </EuiShowFor>
+
+                    <EuiFlexGroup justifyContent="spaceAround">
+                      <EuiFlexItem grow={false}>
+                        <SEMLOOKPLOGO height="170px" width="auto" />
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+
+                    <EuiSpacer size="xl" />
+                    <EuiShowFor sizes={["xl"]}>
+                      <EuiSpacer size="xl" />
+                    </EuiShowFor>
+
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/about" element={<AboutPage />} />
+                      <Route path="/resources" element={<Resources />} />
+                      <Route path="/sample" element={<SamplePage />} />
+                      <Route path="/terms" element={<Term />} />
+                      <Route path="*" element={<Error />} />
+                    </Routes>
+                    <Footer />
+                  </EuiPageContentBody>
+                </EuiPageContent>
+              </EuiPageBody>
+            </EuiPage>
           </Router>
         </QueryClientProvider>
       </EuiProvider>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { EuiFlexItem, EuiPanel, EuiText } from "@elastic/eui";
-import "./Home.css";
+import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiText, EuiSpacer, EuiTitle, EuiLink } from "@elastic/eui";
 import { AutocompleteWidget } from "@km/widgets-semlookp";
 import { useNavigate } from "react-router-dom";
 
@@ -9,36 +8,63 @@ export default function Home() {
   const navigate = useNavigate();
   return (
     <div>
-      <div className="searchbar">
-        <h1>Welcome to the Terminology Service</h1>
-        <EuiFlexItem className="searchBar">
-          <AutocompleteWidget
-            api={"https://semanticlookup.zbmed.de/ols/api/"}
-            onChange={setSearchResults}
-            parameter={"ontology=mesh,efo&type=class"}
-          />
-          {searchResults.map((result) => (
-            <EuiPanel
-              key={result.value}
-              style={{ padding: "10px", margin: "10px" }}
-            >
-              <EuiText
-                onClick={() =>
-                  navigate({
-                    pathname: "/terms",
-                    search: "?iri=" + result.value.iri.toString(),
-                  })
-                }
+      <EuiPanel id="searchBox" hasShadow={true}>
+        <EuiFlexGroup direction="column">
+          <EuiFlexItem>
+            <EuiTitle><h1>Welcome to the Terminology Service</h1></EuiTitle>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <AutocompleteWidget
+              api={"https://semanticlookup.zbmed.de/ols/api/"}
+              onChange={setSearchResults}
+              parameter={"ontology=mesh,efo&type=class"}
+            />
+            {searchResults.map((result) => (
+              <EuiPanel
+                key={result.value}
+                style={{ padding: "10px", margin: "10px" }}
               >
-                {result.label}{" "}
-                {result.value?.description
-                  ? " : " + result.value.description
-                  : ""}
-              </EuiText>
-            </EuiPanel>
-          ))}
-        </EuiFlexItem>
-      </div>
+                <EuiText
+                  onClick={() =>
+                    navigate({
+                      pathname: "/terms",
+                      search: "?iri=" + result.value.iri.toString(),
+                    })
+                  }
+                >
+                  {result.label}{" "}
+                  {result.value?.description
+                    ? " : " + result.value.description
+                    : ""}
+                </EuiText>
+              </EuiPanel>
+            ))}
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                {/* TODO Update example search links once search results page exists */}
+                <EuiText size="s">Examples: <EuiLink href="search?q=diabetes">diabetes</EuiLink>, <EuiLink href="search?q=GO:0098743">GO:0098743</EuiLink></EuiText>
+              </EuiFlexItem>
+              <EuiFlexItem>
+                {/* TODO Update link once resources page exists */}
+                <EuiText size="s" textAlign="right"><EuiLink href="ontologies">Looking for a particular resource?</EuiLink></EuiText>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiPanel>
+      <EuiSpacer size="xl" />
+      <EuiPanel id="aboutSection" hasShadow={true}>
+        <EuiTitle><EuiLink href="docs/index">About the Terminology Service</EuiLink></EuiTitle>
+        <EuiSpacer size="xl" />
+        <EuiText>
+          {/* TODO Update project links (IDSN, NFDI4Health) once project page exists */}
+          <p>The Terminology Service is a repository for biomedical resources that aims to provide a single point of access to the latest ontology and terminology versions. The preliminary health terminology set of the BMBF-funded project <EuiLink href="/ols/projects#idsn">IDSN</EuiLink> was extended by <EuiLink href="/ols/projects#nfdi">NFDI4Health</EuiLink> relevant resources.</p>
+          <p>You can browse the resources through the website as well as programmatically via our API. The API of SemLookP is integrated into other web services for example the ZB MED preprint viewer <EuiLink href="https://preview.zbmed.de/" target="_blank">preVIEW</EuiLink>.</p>
+          <p>It is part of the Semantic Lookup Platform, which is developed and maintained by ZB MED - Information Centre for Life Sciences.</p>
+        </EuiText>
+      </EuiPanel>
     </div>
   );
 }
