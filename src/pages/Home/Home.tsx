5cd8ac6a90a37c9eeb95de5e106600913c2b013d
Vera Clemens <clemens@zbmed.de>
2022-10-14 16:51:20 +0200

    feat: add data content widget to landing page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiText, EuiSpacer, EuiTitle, EuiLink, EuiShowFor } from "@elastic/eui";
-import { AutocompleteWidget } from "@km/widgets-semlookp";
+import { AutocompleteWidget, DataContentWidget } from "@km/widgets-semlookp";
 import { useNavigate, Link } from "react-router-dom";
 import { ReactComponent as SEMLOOKPLOGO } from "../../components/Logos/NFDI_SemLookP_Logo.svg";
 
@@ -20,62 +20,69 @@ export default function Home() {
         <EuiSpacer size="xl" />
       </EuiShowFor>
 
-      <EuiPanel id="searchBox" hasShadow={true}>
-        <EuiFlexGroup direction="column">
-          <EuiFlexItem>
-            <EuiTitle><h1>Welcome to the Terminology Service</h1></EuiTitle>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <AutocompleteWidget
-              api={"https://semanticlookup.zbmed.de/ols/api/"}
-              onChange={setSearchResults}
-              parameter={"ontology=mesh,efo&type=class"}
-            />
-            {searchResults.map((result) => (
-              <EuiPanel
-                key={result.value}
-                style={{ padding: "10px", margin: "10px" }}
-              >
-                <EuiText
-                  onClick={() =>
-                    navigate({
-                      pathname: "/terms",
-                      search: "?iri=" + result.value.iri.toString(),
-                    })
-                  }
-                >
-                  {result.label}{" "}
-                  {result.value?.description
-                    ? " : " + result.value.description
-                    : ""}
-                </EuiText>
-              </EuiPanel>
-            ))}
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiFlexGroup>
+      <EuiFlexGroup>
+        <EuiFlexItem grow={7}>
+          <EuiPanel id="searchBox" hasShadow={true}>
+            <EuiFlexGroup direction="column">
               <EuiFlexItem>
-                <EuiText size="s">Examples: <Link to="/search?q=diabetes">diabetes</Link>, <Link to="/search?q=GO:0098743">GO:0098743</Link></EuiText>
+                <EuiTitle><h1>Welcome to the Terminology Service</h1></EuiTitle>
               </EuiFlexItem>
               <EuiFlexItem>
-                <EuiText size="s" textAlign="right"><Link to="/resources">Looking for a particular resource?</Link></EuiText>
+                <AutocompleteWidget
+                  api={"https://semanticlookup.zbmed.de/ols/api/"}
+                  onChange={setSearchResults}
+                  parameter={"ontology=mesh,efo&type=class"}
+                />
+                {searchResults.map((result) => (
+                  <EuiPanel
+                    key={result.value}
+                    style={{ padding: "10px", margin: "10px" }}
+                  >
+                    <EuiText
+                      onClick={() =>
+                        navigate({
+                          pathname: "/terms",
+                          search: "?iri=" + result.value.iri.toString(),
+                        })
+                      }
+                    >
+                      {result.label}{" "}
+                      {result.value?.description
+                        ? " : " + result.value.description
+                        : ""}
+                    </EuiText>
+                  </EuiPanel>
+                ))}
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiFlexGroup>
+                  <EuiFlexItem>
+                    <EuiText size="s">Examples: <Link to="/search?q=diabetes">diabetes</Link>, <Link to="/search?q=GO:0098743">GO:0098743</Link></EuiText>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiText size="s" textAlign="right"><Link to="/resources">Looking for a particular resource?</Link></EuiText>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
               </EuiFlexItem>
             </EuiFlexGroup>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiPanel>
+          </EuiPanel>
 
-      <EuiSpacer size="xl" />
+          <EuiSpacer size="xl" />
 
-      <EuiPanel id="aboutSection" hasShadow={true}>
-        <EuiTitle><Link to="/docs/index">About the Terminology Service</Link></EuiTitle>
-        <EuiSpacer size="xl" />
-        <EuiText>
-          <p>The Terminology Service is a repository for biomedical resources that aims to provide a single point of access to the latest ontology and terminology versions. The preliminary health terminology set of the BMBF-funded project <Link to="/ols/projects#idsn">IDSN</Link> was extended by <Link to="/ols/projects#nfdi">NFDI4Health</Link> relevant resources.</p>
-          <p>You can browse the resources through the website as well as programmatically via our API. The API of SemLookP is integrated into other web services for example the ZB MED preprint viewer <EuiLink href="https://preview.zbmed.de/" target="_blank">preVIEW</EuiLink>.</p>
-          <p>It is part of the Semantic Lookup Platform, which is developed and maintained by ZB MED - Information Centre for Life Sciences.</p>
-        </EuiText>
-      </EuiPanel>
+          <EuiPanel id="aboutSection" hasShadow={true}>
+            <EuiTitle><Link to="/docs/index">About the Terminology Service</Link></EuiTitle>
+            <EuiSpacer size="xl" />
+            <EuiText>
+              <p>The Terminology Service is a repository for biomedical resources that aims to provide a single point of access to the latest ontology and terminology versions. The preliminary health terminology set of the BMBF-funded project <Link to="/ols/projects#idsn">IDSN</Link> was extended by <Link to="/ols/projects#nfdi">NFDI4Health</Link> relevant resources.</p>
+              <p>You can browse the resources through the website as well as programmatically via our API. The API of SemLookP is integrated into other web services for example the ZB MED preprint viewer <EuiLink href="https://preview.zbmed.de/" target="_blank">preVIEW</EuiLink>.</p>
+              <p>It is part of the Semantic Lookup Platform, which is developed and maintained by ZB MED - Information Centre for Life Sciences.</p>
+            </EuiText>
+          </EuiPanel>
+        </EuiFlexItem>
+        <EuiFlexItem grow={3}>
+          <DataContentWidget api="https://semanticlookup.zbmed.de/ols/api/" />
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </>
   );
 }
