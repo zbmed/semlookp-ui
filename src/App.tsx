a634605ab966533e0adb8ef9adbbcb2fbd932e69
Roman Baum <baum@zbmed.de>
2022-10-22 00:32:11 +0200

    refactor: reordered import and routes in ascending alphabetical order.

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,10 @@ import { EuiProvider } from "@elastic/eui";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import Layout from "./components/Layout/Layout";
-import Home from "./pages/Home/Home";
-import AboutPage from "./pages/About/AboutPage";
-import Resources from "./pages/Resources/Resources";
-import Error from "./pages/Error/Error";
-import Term from "./pages/Term/Term";
-import ProjectsPage from './pages/Projects/ProjectsPage'
+import {
+  Home, AboutPage, ApiPage, Error, ImprintPage, PrivacyPage, ProjectsPage,
+  Resources, Term
+} from "./PagesImport";
 
 function App() {
   const queryClient = new QueryClient();
@@ -23,10 +21,13 @@ function App() {
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
               <Route path="/about" element={<AboutPage />} />
-              <Route path="/resources" element={<Resources />} />
-              <Route path="/projects" element={<ProjectsPage />} />
-              <Route path="/terms" element={<Term />} />
+              <Route path="/about/api" element={<ApiPage />} />
               <Route path="*" element={<Error />} />
+              <Route path="/imprint" element={<ImprintPage />} />
+              <Route path="/privacy" element={<PrivacyPage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/resources" element={<Resources />} />
+              <Route path="/terms" element={<Term />} />
             </Route>
           </Routes>
         </Router>
