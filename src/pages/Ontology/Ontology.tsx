e037753c5378e8a3595c652883f70585d2fbe7ef
Julian Schneider <schneider@zbmed.de>
2022-11-07 15:17:00 +0100

    feat(Ontology): include Title+Description widgets
    +update other widget usage

--- src/pages/Ontology/Ontology.tsx
+++ src/pages/Ontology/Ontology.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { EuiFlexGroup, EuiFlexItem, EuiPanel } from "@elastic/eui";
 import { useParams, useNavigate } from "react-router-dom";
-import { AutocompleteWidget, HierarchyTabWidget, OntologyInfoWidget, JsonApiWidget } from "@km/widgets-semlookp";
+import { AutocompleteWidget, HierarchyTabWidget, OntologyInfoWidget, JsonApiWidget, DescriptionWidget, TitleWidget } from "@km/widgets-semlookp";
 
 
 export default function Ontology() {
   const routeParams = useParams();
   const navigate = useNavigate();
+  const API = "https://semanticlookup.zbmed.de/ols/api/"
 
   return (
     <>
       <EuiFlexGroup justifyContent="spaceAround">
         <EuiFlexItem grow={7}>
           <EuiPanel id="titleBox" hasShadow={true}>
-            PLACEHOLDER! Will contain 2 widgets: Title and Description {/*TODO*/}
+            <TitleWidget api={API} objType={"ontology"} ontologyID={routeParams.ontoId}></TitleWidget>
+            <DescriptionWidget api={API} objType={"ontology"} ontologyID={routeParams.ontoId}></DescriptionWidget>
           </EuiPanel>
         </EuiFlexItem>
         <EuiFlexItem grow={3}>
           <EuiPanel id="searchBox" hasShadow={true}>
             <AutocompleteWidget
-              api={"https://semanticlookup.zbmed.de/ols/api/"}
-              onChange={(result: {options:[], selectedOption}) => {
+              api={API}
+              selectionChangedEvent={(selectedOption) => {
                 navigate("/ontologies/"+routeParams.ontoId +
-                  "/terms?iri="+encodeURI(result.selectedOption.value.iri))
+                  "/terms?iri="+encodeURI(selectedOption.iri))
               }}
               parameter={"ontology="+routeParams.ontoId}
             />
             <JsonApiWidget
-              apiQuery={"https://semanticlookup.zbmed.de/ols/api/ontologies/"+routeParams.ontoId}
+              apiQuery={API+"ontologies/"+routeParams.ontoId}
               buttonText={"JSON"}
               buttonSize={"s"}
             />
@@ -39,12 +41,12 @@ export default function Ontology() {
       <EuiFlexGroup>
         <EuiFlexItem grow={7}>
           <EuiPanel id="hierarchyBox" hasShadow={true}> {/*TODO initially open root element(s) instead of placeholder iri*/}
-            <HierarchyTabWidget linkToSelf={"https://semanticlookup.zbmed.de/ols/api/ontologies/"+routeParams.ontoId+"/terms/"} iri={"https://identifiers.org/atc:P02"}/>
+            <HierarchyTabWidget linkToSelf={API+"ontologies/"+routeParams.ontoId+"/terms/"} iri={"https://identifiers.org/atc:P02"}/>
           </EuiPanel>
         </EuiFlexItem>
         <EuiFlexItem grow={3}>
           <EuiPanel id="ontoInfoBox" hasShadow={true}>
-            <OntologyInfoWidget api={"https://semanticlookup.zbmed.de/ols/api/"} onto={routeParams.ontoId}/>
+            <OntologyInfoWidget api={API} ontologyID={routeParams.ontoId}/>
           </EuiPanel>
         </EuiFlexItem>
       </EuiFlexGroup>
