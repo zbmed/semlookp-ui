a6833d3bf7f3d377fdf1374ad6ae6c6e567fa72d
Julian Schneider <schneider@zbmed.de>
2023-01-25 17:50:41 +0100

    feat: indicate parameters of AutocompleteWidget to the user

--- src/pages/Entity/Entity.tsx
+++ src/pages/Entity/Entity.tsx
@@ -100,6 +100,7 @@ export default function Entity() {
                                     {/*TODO Add on click event*/}
                                     <AutocompleteWidget
                                         api={API}
+                                        placeholder={"Jump to an entity in this ontology"}
                                         selectionChangedEvent={goToEntityPage}
                                         parameter={"ontology=" + routeParams.ontologyId}
                                         frontend={"nfdi4health"}
--- src/pages/Ontology/Ontology.tsx
+++ src/pages/Ontology/Ontology.tsx
@@ -51,6 +51,7 @@ export default function Ontology() {
           <EuiPanel id="searchBox" hasShadow={true}>
             <AutocompleteWidget
               api={API}
+              placeholder={"Jump to an entity in this ontology"}
               selectionChangedEvent={goToEntityPage}
               parameter={"ontology="+routeParams.ontologyId}
               frontend={"nfdi4health"}
