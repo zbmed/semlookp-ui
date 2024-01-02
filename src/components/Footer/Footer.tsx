d0df0c27f786f42532af3ef9d4ec87eae017bf5e
sasse <sasse@zbmed.de>
2024-01-02 13:46:14 +0100

    refactor: ui improvement

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { EuiFlexGroup, EuiFlexItem, EuiText, EuiLink, EuiSpacer, EuiHorizontalRule } from "@elastic/eui";
-import { ReactComponent as DfgLogo } from "../Logos/dfg_logo_schriftzug_blau_foerderung_en.svg";
-import { ReactComponent as NfdiLogo } from "../Logos/NFDI4Health_Logo_cmyk_RZ.svg";
-import { ReactComponent as ZbmedLogo } from "../Logos/ZBMED_2017_DE.svg";
-import EuiCustomLink from '../../router/EuiCustomLink';
+import { EuiFlexGroup, EuiFlexItem, EuiHorizontalRule, EuiLink, EuiSpacer, EuiText } from "@elastic/eui";
+import { ReactComponent as DfgLogo } from "../Layout/logos/dfg_logo_schriftzug_blau_foerderung_en.svg";
+import { ReactComponent as NfdiLogo } from "../Layout/logos/NFDI4Health_Logo_cmyk_RZ.svg";
+import { ReactComponent as ZbmedLogo } from "../Layout/logos/ZBMED_2017_DE.svg";
+import EuiCustomLink from "../../router/EuiCustomLink";
 
 export default function Footer() {
   return (
@@ -52,7 +52,7 @@ export default function Footer() {
         <EuiFlexGroup justifyContent="spaceAround" gutterSize="xl">
           <EuiFlexItem grow={false}>
             <EuiText color="subdued" size="s">
-              This service is developed in the project Placeholder. (Project identifier 0123456789).
+              This service is developed in the project NFDI4Health which is one of the funded consortia of the National Research Data Infrastructure programme of the DFG. (Project identifier 442326535).
             </EuiText>
           </EuiFlexItem>
         </EuiFlexGroup>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,59 @@
 import React from "react";
-import { EuiCard, EuiFlexGroup, EuiFlexItem, EuiLink, EuiPanel, EuiSpacer, EuiText, EuiTitle } from "@elastic/eui";
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiImage,
+  EuiLink,
+  EuiPanel,
+  EuiSpacer,
+  EuiText,
+  EuiTextColor,
+  EuiTitle
+} from "@elastic/eui";
 import { AutocompleteWidget, DataContentWidget } from "@nfdi4health/semlookp-widgets";
 import { useNavigate } from "react-router-dom";
 import EuiCustomLink from "../../router/EuiCustomLink";
 import { navigateToEntity } from "../../index";
 import GlobalConfig from "../../config";
 import "../../index.css";
+import widgetimage from "../../components/Layout/images/widgets-icons.png";
+import { ReactComponent as NFDI4HEALTHLOGO } from "../../components/Layout/logos/NFDI4Health_Logo_cmyk_RZ.svg";
 
 export default function Home() {
   const navigate = useNavigate();
 
+
   return (
     <>
-      {/*<EuiFlexGroup>*/}
+      <EuiFlexGroup justifyContent="spaceAround">
+        <EuiFlexItem grow={false}>
+          <NFDI4HEALTHLOGO height="170px" width="auto" />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer size="xl" />
+      
+      <EuiFlexGroup direction={"row"} gutterSize="m" alignItems={"center"}>
+        <EuiFlexItem grow={false}>
+          <EuiTitle size="m">
+            <h1><EuiTextColor>Welcome to the Semantic Lookup Platform</EuiTextColor></h1>
+          </EuiTitle>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <span className="brandColorSemlookp" style={{ fontSize: "xxx-large" }}>
+            SEM<span className="brandSubColorSemLookp">LOOK</span>P</span>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer size="l" />
+
       <EuiFlexItem>
         <EuiPanel id="searchBox" hasShadow={true}>
           <EuiFlexGroup direction="column">
-            <EuiFlexItem>
-              <EuiTitle><h1>Welcome to the Terminology Service SemLookP</h1></EuiTitle>
-            </EuiFlexItem>
+            <EuiFlexItem><EuiTitle size={"s"}><EuiTextColor>Search the Terminology
+              Service</EuiTextColor></EuiTitle></EuiFlexItem>
             <EuiFlexItem>
               <AutocompleteWidget
                 api={GlobalConfig.apiUrlGateway}
-                placeholder={"Search"}
+                placeholder={"Type to search"}
                 selectionChangedEvent={(selectedOption) => {
                   navigateToEntity(selectedOption, navigate);
                 }}
@@ -39,7 +70,7 @@ export default function Home() {
             </EuiFlexItem>
             <EuiFlexItem>
               <EuiFlexGroup>
-                <EuiFlexItem grow={2}>
+                <EuiFlexItem grow={3}>
                   <EuiText size="s">Or use the <EuiCustomLink to="/search?q=*">Advanced Search</EuiCustomLink>
                   </EuiText>
                 </EuiFlexItem>
@@ -57,23 +88,48 @@ export default function Home() {
 
         <EuiFlexGroup>
           <EuiFlexItem grow={3}>
-            <DataContentWidget
-              api={GlobalConfig.apiUrlGateway}
-              parameter={"collection=nfdi4health"}
-            />
-          </EuiFlexItem>
-          <EuiFlexItem grow={4}>
-            <EuiCard
-              title="Use Case"
-              description={""}
-              layout="horizontal"
+
+            <EuiPanel
+              grow={true}
+              hasShadow={false}
+              style={{ backgroundColor: "#ccedf5", minHeight: 150 }}
             >
+              <EuiFlexGroup direction={"column"} gutterSize={"none"}>
+                <DataContentWidget
+                  api={GlobalConfig.apiUrlGateway}
+                  parameter={"collection=nfdi4health"}
+                />
+              </EuiFlexGroup>
+
+            </EuiPanel>
+          </EuiFlexItem>
+
+          <EuiFlexItem grow={3}>
+            <EuiPanel
+              grow={true}
+              hasShadow={false}
+              style={{ backgroundColor: "#cce4f5", minHeight: 150 }}
+            >
+              <EuiFlexGroup>
+                {" "}
+                <EuiFlexItem>
+                  <EuiTitle size="s">
+                    <h3>
+                      <EuiTextColor> Use Case </EuiTextColor>
+                    </h3>
+                  </EuiTitle>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+              <EuiSpacer size="s" />
               <EuiText>
-                <i><EuiLink href={"https://www.zbmed.de/en/"}>ZB MED </EuiLink>
-                  preprint Viewer </i>
-                <EuiLink href="https://preview.zbmed.de/"><span className="brandColor" style={{ fontSize: "x-large" }}>
-                  pre<span className="brandSubColor">VIEW</span>: COVID-19
+                <EuiLink href={"https://www.zbmed.de/en/"}> ZB MED </EuiLink>
+                preprint Viewer <br></br>
+                <EuiSpacer size="s" />
+                <EuiLink href="https://preview.zbmed.de/"><span className="brandColorPreview"
+                                                                style={{ fontSize: "x-large" }}>
+                  pre<span className="brandSubColorPreview">VIEW</span>: COVID-19
                 </span></EuiLink><br></br>
+                <EuiSpacer size="s" />
                 <i>Semantic Search to Explore COVID-19 Research Preprints.</i><br></br>
                 <EuiSpacer size="s" />
                 The search engine uses the
@@ -81,32 +137,61 @@ export default function Home() {
                 <EuiLink href={"https://github.com/nfdi4health/semlookp-widgets"}> Widgets </EuiLink>
                 to display semantic information.
               </EuiText>
-            </EuiCard>
+            </EuiPanel>
+
           </EuiFlexItem>
           <EuiFlexItem grow={3}>
-            <EuiCard title="SemLookP Widgets"
-              description={""}
-              layout="horizontal">
-
-            </EuiCard>
+            <EuiPanel
+              grow={true}
+              hasShadow={false}
+              style={{ backgroundColor: "#D3DAE6", minHeight: 150 }}
+            >
+              <EuiFlexGroup>
+                {" "}
+                <EuiFlexItem>
+                  <EuiTitle size="s">
+                    <h3>
+                      <EuiTextColor> SemLookP Widgets </EuiTextColor>
+                    </h3>
+                  </EuiTitle>
+                  <EuiSpacer size="s" />
+                  <EuiText>
+                    <i>Small GUI components to use and display semantic information.</i><br></br>
+                    <EuiSpacer size="s" />
+                    <EuiImage
+                      size="original"
+                      hasShadow
+                      allowFullScreen
+                      caption=""
+                      src={widgetimage}
+                      alt={"metadata"}
+                    />
+                    The <EuiLink href={"https://github.com/nfdi4health/semlookp-widgets"}> Widgets </EuiLink> are based
+                    on the
+                    <EuiCustomLink to={"/about/api"}> SemLookP API</EuiCustomLink>.
+                  </EuiText>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiPanel>
           </EuiFlexItem>
         </EuiFlexGroup>
 
-        <EuiSpacer size="xl" />
+        <EuiSpacer size="xxl" />
 
         {/*<EuiPanel id="aboutSection" hasShadow={true}>*/}
-        <EuiTitle><EuiCustomLink to="/docs/index">About the Terminology Service</EuiCustomLink></EuiTitle>
-        <EuiSpacer size="xl" />
+        <EuiTitle><EuiCustomLink to="/docs/index">About</EuiCustomLink></EuiTitle>
+        <EuiSpacer size="m" />
         <EuiText>
-          <p>The Terminology Service is a repository for biomedical resources that aims to provide a single point of
-            access to the latest ontology and terminology versions. The preliminary health terminology set of the
-            BMBF-funded project <EuiCustomLink to="/projects#idsn">IDSN</EuiCustomLink> was extended by <EuiCustomLink
-              to="/projects#nfdi">NFDI4Health</EuiCustomLink> relevant resources.</p>
-          <p>You can browse the resources through the website as well as programmatically via our API. The API of
-            SemLookP is integrated into other web services for example the ZB MED preprint viewer <EuiLink
-              href="https://preview.zbmed.de/" target="_blank">preVIEW</EuiLink>.</p>
-          <p>It is part of the Semantic Lookup Platform, which is developed and maintained by ZB MED - Information
-            Centre for Life Sciences.</p>
+          <p>The Terminology Service is a repository for controlled resources that aims to provide a single point of
+            access to the latest ontology and terminology versions.
+            You can browse the resources through the website as well as programmatically via the API.
+            It is part of the Semantic Lookup Platform, which is developed and maintained by
+            <EuiLink href={"https://www.zbmed.de/en/"}> ZB MED - Information Centre for Life Sciences.</EuiLink>
+            The system is based on the Ontology Lookup Service (OLS), that is developed and maintained by the
+            <EuiLink href={"https://www.ebi.ac.uk/about/teams/samples-phenotypes-ontologies/"}
+            > Samples, Phenotypes and Ontologies Team (SPOT) </EuiLink> at
+            <EuiLink href={"https://www.ebi.ac.uk/"}> EMBL-EBI. </EuiLink>
+          </p>
         </EuiText>
         {/*</EuiPanel>*/}
       </EuiFlexItem>
