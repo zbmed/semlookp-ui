352be68d1fa020610480919388f99d424e8ec12b
sasse <sasse@zbmed.de>
2024-03-13 16:06:51 +0100

    fix(apiurl): add ols4 api url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const GlobalConfig = {
   apiUrlGateway: 'https://semanticlookup.zbmed.de/api/',
   apiUrlSemlookp:  'https://semanticlookup.zbmed.de/ols/api/',
-  apiUrlSemlookpOls4: 'http://ols4.qa.km.k8s.zbmed.de/ols4/api/',
+  apiUrlSemlookpOls4: 'https://ols4-nfdi4health.prod.km.k8s.zbmed.de/ols4/api/',
   apiUrlEbi: 'https://www.ebi.ac.uk/ols4/api/'
 };
 
