91ed85985eabb9240b115ebc495ccaf6b862c0f4
sasse <sasse@zbmed.de>
2024-06-28 11:16:20 +0200

    refactor: use https api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const GlobalConfig = {
-  apiUrlOls4: "http://ols4-nfdi4health.prod.km.k8s.zbmed.de/ols4/api/"
+  apiUrlOls4: "https://ols4-nfdi4health.prod.km.k8s.zbmed.de/ols4/api/"
 };
 
 export default GlobalConfig;
