be578b61cf0a93490e70398cbad5c9996b5bae9a
sasse <sasse@zbmed.de>
2024-06-28 08:26:27 +0200

    refactor: change api and dns name

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const GlobalConfig = {
-  apiUrlOls4: "http://ols4-nfdi4health.qa.km.k8s.zbmed.de/ols4/api/"
+  apiUrlOls4: "http://ols4-nfdi4health.prod.km.k8s.zbmed.de/ols4/api/"
 };
 
 export default GlobalConfig;
