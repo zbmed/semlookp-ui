5256f70829b06c66d18155c2a5a20fc5a555aa8c
sasse <sasse@zbmed.de>
2024-10-22 15:49:34 +0200

    docs: update readme

--- src/imageMap.js
+++ src/imageMap.js
@@ -21,6 +21,10 @@ export const imageMap = {
       import(
         "./defaultTemplate/defaultTemplateLayout/defaultTemplateImages/badge-svgrepo-com.svg"
       ),
+    infoBoxLogoA: () =>
+      import(
+        "./defaultTemplate/defaultTemplateLayout/defaultTemplateImages/aquarius-svgrepo-com.svg"
+      ),
     infoBoxLogoB: () => import("./common/layout/images/widgets-icons.png"),
     ts4nfdiLogo: () => import("./common/layout/images/TS4_icon.svg"),
     projectALogo: () =>
