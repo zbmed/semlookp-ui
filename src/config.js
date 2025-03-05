326a94f6fb0f5226e8ce0728999e2e3b3f44e151
sasse <sasse@zbmed.de>
2025-03-05 10:09:43 +0100

    config: change OLS backend url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // health
 export const global_config = {
-  api_url: "https://ols4-nfdi4health.prod.km.k8s.zbmed.de/ols4/api/",
+  api_url: "https://semanticlookup.zbmed.de/ols/api/",
   projectName: "health",
 };
 
