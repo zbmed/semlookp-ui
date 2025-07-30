0f7e8dbb92990a7ccb97f6b053f9af43d5c7b282
jusa3 <sasse@zbmed.de>
2025-07-30 08:05:36 +0200

    fix: change Matomo integration

--- src/App.tsx
+++ src/App.tsx
@@ -14,67 +14,60 @@ import Projects from "./common/pages/Projects";
 import { default as Resources } from "./common/pages/Resources";
 import { default as SearchResults } from "./common/pages/SearchResults";
 import StaticMarkdownPage from "./common/pages/static/StaticMarkdownPage";
-import { global_config, ts_specific_metadata } from "./config";
+import { ts_specific_metadata } from "./config";
 
-const shouldUseMatomo = global_config.projectname === "health";
-
-const instance = shouldUseMatomo
-  ? createInstance({
-      urlBase: "https://books.publisso.de/matomo/",
-      trackerUrl: "https://books.publisso.de/matomo/matomo.php", // optional, default value: `${urlBase}matomo.php`
-      srcUrl: "https://books.publisso.de/matomo/matomo.js", // optional, default value: `${urlBase}matomo.js`
-      siteId: 16,
-      linkTracking: false, // Important!
-    })
-  : null;
+const instance = createInstance({
+  urlBase: "https://books.publisso.de/matomo/",
+  trackerUrl: "https://books.publisso.de/matomo/matomo.php", // optional, default value: `${urlBase}matomo.php`
+  srcUrl: "https://books.publisso.de/matomo/matomo.js", // optional, default value: `${urlBase}matomo.js`
+  siteId: 16,
+  linkTracking: false, // Important!
+});
 
 function App() {
-  const app = (
-    <Router basename="/">
-      <LinkScroller>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/about/api" element={<ApiPage />} />
-              <Route path="*" element={<Error />} />
-              <Route
-                path="/privacy"
-                element={
-                  <StaticMarkdownPage
-                    file={ts_specific_metadata.homepage.privacy_markdown_file}
-                    title={"Privacy Policy"}
-                  />
-                }
-              />
-              <Route
-                path="/imprint"
-                element={
-                  <StaticMarkdownPage
-                    file={ts_specific_metadata.homepage.imprint_markdown_file}
-                    title={"Imprint"}
-                  />
-                }
-              />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/resources" element={<Resources />} />
-              <Route path="/search" element={<SearchResults />} />
-              <Route
-                path="/ontologies/:ontologyId/:entityType"
-                element={<Entity />}
-              />
-              <Route path="/ontologies/:ontologyId" element={<Ontology />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </LinkScroller>
-    </Router>
-  );
-  return shouldUseMatomo ? (
-    <MatomoProvider value={instance}>{app}</MatomoProvider>
-  ) : (
-    app
+  return (
+    <MatomoProvider value={instance}>
+      <Router basename="/">
+        <LinkScroller>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/about/api" element={<ApiPage />} />
+                <Route path="*" element={<Error />} />
+                <Route
+                  path="/privacy"
+                  element={
+                    <StaticMarkdownPage
+                      file={ts_specific_metadata.homepage.privacy_markdown_file}
+                      title={"Privacy Policy"}
+                    />
+                  }
+                />
+                <Route
+                  path="/imprint"
+                  element={
+                    <StaticMarkdownPage
+                      file={ts_specific_metadata.homepage.imprint_markdown_file}
+                      title={"Imprint"}
+                    />
+                  }
+                />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/resources" element={<Resources />} />
+                <Route path="/search" element={<SearchResults />} />
+                <Route
+                  path="/ontologies/:ontologyId/:entityType"
+                  element={<Entity />}
+                />
+                <Route path="/ontologies/:ontologyId" element={<Ontology />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </LinkScroller>
+      </Router>
+    </MatomoProvider>
   );
 }
 
